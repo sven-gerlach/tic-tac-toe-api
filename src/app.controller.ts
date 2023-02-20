d9ee18227536af37d71111c961f5d2954b8c158c
sven-gerlach <svengerlach@protonmail.com>
2023-02-20 10:27:52 -0500

    Add hearbeat route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @HttpCode(200)
-  @Get()
+  @Get('heartbeat')
   heartbeat(): string {
     return this.appService.heartbeat();
   }
